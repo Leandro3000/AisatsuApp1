422b3634c6fab57eb7f86816acb421237d806ee1
Leandro <moji.br.14@icloud.com>
2019-08-29 20:50:59 +0900

    確認お願いします。

--- app/src/main/java/com/example/aisatsuapp/MainActivity.kt
+++ app/src/main/java/com/example/aisatsuapp/MainActivity.kt
@@ -5,52 +5,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 import java.sql.Time
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener(this)
-        button2.setOnClickListener(this)
-
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.button1) {
-            textView.text = editText.text.toString()
-        } else if (v.id == R.id.button2) {
-            showAlertDialog()
-        }
-        button1.setOnClickListener ( { view: View? ->
-            Log.d("UI_PARTS", "ボタンをタップしました")
-        })
-    }
-
-    private fun showAlertDialog() {
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        alertDialogBuilder.setTitle("タイトル")
-        alertDialogBuilder.setMessage("メッセージ")
-
-    }
-
-        val alertDialog = showAlertDialog().create()
-       aler
+    override  fun onClick(v: View) {
+        showTimePickerDialog()
     }
 
     private fun showTimePickerDialog() {
         val timePickerDialog = TimePickerDialog(
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-                Log.d("UI_PARTS", "$hour:$minute")
-            },
-            13, 0, true)
+
+                if (2 <= hour && hour < 10) {
+
+                    textView.text = "おはよう"
+
+                } else if (10 <= hour && hour < 18) {
+
+                    textView.text = "こんにちは"
+
+                } else {
+
+                    textView.text = "こんばんは"
+
+                }
+
+
+            },13,0,true
+
+        )
         timePickerDialog.show()
+
     }
-}
 }
 
 
